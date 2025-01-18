ba7ba5fde3398f8eb72bef545f86417ca31c82d6
Nikitha Matta <100593166+NikithaMatta@users.noreply.github.com>
2025-01-18 13:45:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (71.78%) - LeetSync

--- POTD/Noob/1603-running-sum-of-1d-array/running-sum-of-1d-array.java
+++ POTD/Noob/1603-running-sum-of-1d-array/running-sum-of-1d-array.java
@@ -1,11 +1,8 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int[] sum = new int[nums.length];
-        int s = 0;
-        for (int i = 0; i < nums.length; i++) {
-            s += nums[i];
-            sum[i] = s;
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1]; 
         }
-        return sum;
+        return nums;
     }
 }
