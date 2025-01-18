e2b8c5d884c3a0f230db56bdb98c9fe5389ae39d
Nikitha Matta <100593166+NikithaMatta@users.noreply.github.com>
2025-01-18 14:25:30 +0530

    Time: 2 ms (82.94%) | Memory: 44.3 MB (98.75%) - LeetSync

--- POTD/Noob/383-ransom-note/ransom-note.java
+++ POTD/Noob/383-ransom-note/ransom-note.java
@@ -2,14 +2,13 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         if (ransomNote.length() > magazine.length()) 
             return false;
-        int[] alphabets_counter = new int[26];
-        for (char c : magazine.toCharArray())
-            alphabets_counter[c-'a']++;
 
-        for (char c : ransomNote.toCharArray()){
-            if (alphabets_counter[c-'a'] == 0) 
+        int[] charCount = new int[26];
+        for (char c : magazine.toCharArray())
+            charCount[c - 'a']++;
+        for (char c : ransomNote.toCharArray()) {
+            if (--charCount[c - 'a'] < 0)
                 return false;
-            alphabets_counter[c-'a']--;
         }
         return true;
     }
